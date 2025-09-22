b6f4dd6529eeabee41dc7dc204b76ffb0020333d
Shreeya1904 <shreeya1904@gmail.com>
2025-09-23 03:06:10 +0530

    bugs

--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -3,5 +3,17 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_URL || '/'
+  base: process.env.VITE_BASE_URL || '/',
+
+  build: {
+    chunkSizeWarningLimit: 2000, // optional: raise warning limit
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          vendor: ['axios', 'lodash'], // add heavy deps here
+        },
+      },
+    },
+  },
 })
